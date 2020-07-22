891987544fa02ed20c345dc7882eae9b57dcc39b
miki31 <it.misha2016@gmail.com>
2020-07-22 22:00:50 +0300

    Install Angular App

--- src/main/java/com/wallet/AppWallet.java
+++ src/main/java/com/wallet/AppWallet.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppWallet {
 
+    // https://www.baeldung.com/spring-boot-angular-web
+
     public static void main(String[] args) {
         SpringApplication.run(AppWallet.class, args);
     }
